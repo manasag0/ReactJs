48416cf5f080bb601264f9d0bf87a1e25fc6ac4f
Manasa G <manasa.g7890@hotmail.com>
2023-11-18 21:38:49 +0530

    modified

---
+++ tailwindcustom/src/components/Card.jsx
@@ -0,0 +1,10 @@
+
+function Card() {
+  return (
+    <div>
+      <h1 className='text-green-500 justify-center align-middle bg-yellow-600 rounded-md' >Hello, there you go !!</h1>
+    </div>
+  )
+}
+
+export default Card
