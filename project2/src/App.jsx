f69ffcddf4e2de3de8469def0708b3d4feafa1db
Manasa G <manasa.g7890@hotmail.com>
2023-11-18 19:41:56 +0530

    several changes

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -5,7 +5,11 @@ function App() {
 
   return (
     <>
-     <h1 className="text-green-700 bg-orange-600 ">Hello Manasa</h1>
+
+    <h1 className="bg-">
+      Hello world!
+    </h1>
+
     </>
   )
 }
