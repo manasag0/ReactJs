413b100c068e32e7c15dc4fd34f24a58f5544791
Manasa G <manasa.g7890@hotmail.com>
2023-11-19 12:04:37 +0530

    test app

---
+++ my-project/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl bg-slate-500 font-bold">
+      hello manasa
+    </h1>
+    </>
+  )
+}
+
+export default App
