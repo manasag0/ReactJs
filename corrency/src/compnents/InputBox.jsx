7e8eb2fec98352fb7124860c3971d4c3c0b3d06d
Manasa G <manasa.g7890@hotmail.com>
2023-11-21 21:36:42 +0530

    App.jsx modified

--- corrency/src/compnents/InputBox.jsx
+++ corrency/src/compnents/InputBox.jsx
@@ -13,16 +13,22 @@ function InputBox({
     className=""
 }) {
   return (
-    <div className={` bg-white p-3 rounded-lg text-sm flex ${className}`}>
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1-2">
         <label className="text-black/40 mb-2 inline-block" >
+        {label}
+        </label>
             <input 
             type="number" 
-            className="outline-none w-full"
+            className="outline-none w-full bg-transparent py-1.5"
+            placeholder="Amount"
             value={amount}
-            disabled={amoundDisabled} />
-            {label}
-        </label>
+            disabled={amoundDisabled}
+            onChange={(e)=>onAmountChange && onAmountChange(Number(e.target.value))}  />
+
+      </div>
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p>Currency Type</p>
       </div>
     </div>
   )
